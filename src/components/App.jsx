a45c4c5d2b97320044d89561c4b4a4027e120258
Kirill <94064326+Fantom19@users.noreply.github.com>
2023-05-24 20:55:19 +0300

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,70 +5,62 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from "./App.module.css"
 
-const App = () => {
-const [contacts, setContacts] = useState([{ id: 'id-1', name: 'Go IT', number: '050 366 17 77' }]);
-const [filter, setFilter] = useState('');
+     const App = () => {
+   const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? []);
+   const [filter, setFilter] = useState('');
 
-useEffect(() => {
-const storedContacts = localStorage.getItem('contacts');
-if (storedContacts) {
-setContacts(JSON.parse(storedContacts));
-}
-}, []);
+      useEffect(() => {
+   localStorage.setItem('contacts', JSON.stringify(contacts));
+      }, [contacts]);
 
-useEffect(() => {
-localStorage.setItem('contacts', JSON.stringify(contacts));
-}, [contacts]);
+       const addNewContact = (name, number) => {
+    const newContact = { name, number, id: nanoid() };
+    const isDuplicateContact = contacts.some(
+      contact =>
+    contact.name.toLowerCase() === name.toLowerCase() ||
+    contact.number === number
+    );if (isDuplicateContact) {
+     alert(`${name} is already in contacts`);
+         return;}
 
-const addNewContact = (name, number) => {
-const newContact = { name, number, id: nanoid() };
-const isDuplicateContact = contacts.some(
-contact =>
-contact.name.toLowerCase() === name.toLowerCase() ||
-contact.number === number
-);if (isDuplicateContact) {
-  alert(`${name} is already in contacts`);
-  return;
-}
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
 
-setContacts(prevContacts => [...prevContacts, newContact]);
-};
+      const searchFilter = filter => {
+      setFilter(filter);
+        };
 
-const searchFilter = filter => {
-setFilter(filter);
-};
+     const filteredContacts = contacts.filter(contact =>
+     contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
 
-const filteredContacts = contacts.filter(contact =>
-contact.name.toLowerCase().includes(filter.toLowerCase())
-);
+       const removeContact = id => {
+       setContacts(prevContacts =>
+     prevContacts.filter(contact => contact.id !== id));
+       };
 
-const removeContact = id => {
-setContacts(prevContacts =>
-prevContacts.filter(contact => contact.id !== id)
-);
-};
+   return (
+          <div
+          style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          paddingLeft: '50px',
+          fontSize: 20,
+          color: '#010101',
+          }}
+          >
 
-return (
-<div
-style={{
-height: '100vh',
-display: 'flex',
-flexDirection: 'column',
-paddingLeft: '50px',
-fontSize: 20,
-color: '#010101',
-}}
->
-<h1 className={css.titel}>Phonebook</h1>
-<ContactForm addNewContact={addNewContact} />
-<h2 className={css.titel}>Contacts</h2>
-<Filter searchFilter={searchFilter} />
-<ContactList
-     contacts={filteredContacts}
-     removeContact={removeContact}
-   />
-</div>
-);
-};
+     <h1 className={css.titel}>Phonebook</h1>
+     <ContactForm addNewContact={addNewContact} />
+     <h2 className={css.titel}>Contacts</h2>
+     <Filter searchFilter={searchFilter} />
+     <ContactList
+          contacts={filteredContacts}
+          removeContact={removeContact}
+        />
+     </div>
+     );
+     };
 
-export default App;
+     export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,34 +2,33 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactForm.module.css';
 
-const ContactForm = ({ addNewContact }) => {
-const [name, setName] = useState('');
-const [number, setNumber] = useState('');
+    const ContactForm = ({ addNewContact }) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-const handleChange = e => {
-if (e.target.name === 'name') {
-setName(e.target.value);
-} else if (e.target.name === 'number') {
-setNumber(e.target.value);
-}
-};
+     const handleChange = e => {
+      if (e.target.name === 'name') {
+        setName(e.target.value);
+       } else if (e.target.name === 'number') {
+        setNumber(e.target.value);
+      } };
 
-const handleSubmit = e => {
-e.preventDefault();
-addNewContact(name, number);
-reset();
-};
+     const handleSubmit = e => {
+       e.preventDefault();
+       addNewContact(name, number);
+       reset();
+      };
 
-const reset = () => {
-setName('');
-setNumber('');
-};
+      const reset = () => {
+      setName('');
+      setNumber('');
+      };
 
-return (
-<form className={css.form} onSubmit={handleSubmit}>
-<label className={css.formLabel}>
-Name
-<input
+   return (
+      <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.formLabel}>
+         Name
+      <input
        placeholder='Enter a name'
        className={css.formInput}
        value={name}
@@ -39,11 +38,11 @@ Name
        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
        required
        onChange={handleChange}
-     />
-</label>
-<label className={css.formLabel}>
-Number
-<input 
+       />
+     </label>
+     <label className={css.formLabel}>
+         Number
+     <input 
        placeholder='Enter a number'
        className={css.formInput}
        value={number}
@@ -54,16 +53,15 @@ Number
        required
        onChange={handleChange}
      />
-</label>
-<button className={css.formButton} type="submit">
-Add contact
-</button>
-</form>
-);
-};
+     </label>
+     <button className={css.formButton} type="submit">
+         Add contact
+     </button>
+     </form> );
+   };
 
-ContactForm.propTypes = {
-addNewContact: PropTypes.func.isRequired,
-};
+  ContactForm.propTypes = {
+  addNewContact: PropTypes.func.isRequired,
+ };
 
 export default ContactForm;
